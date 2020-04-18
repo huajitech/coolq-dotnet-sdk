2825b3aad1cecc8edb9984b284531fc009a4d636
SYC <yc.s@outlook.com>
2020-04-18 15:50:45 +0800

    修复在初始化时调用酷Q API 的 BUG。

--- HuajiTech.CoolQ/Bot.cs
+++ HuajiTech.CoolQ/Bot.cs
@@ -19,6 +19,11 @@ namespace HuajiTech.CoolQ
         internal const string ApiVersion = "9";
         private static readonly ConstructorInfo AppConstructor;
 
+        private static bool? _canSendImage;
+        private static bool? _canSendRecord;
+        private static CurrentUser _currentUser;
+        private static DirectoryInfo _dataDirectory;
+
         static Bot()
         {
             AppDomain.CurrentDomain.AssemblyResolve +=
@@ -31,19 +36,20 @@ namespace HuajiTech.CoolQ
         /// <summary>
         /// 获取一个值，指示是否可以发送图片。
         /// </summary>
-        public static bool CanSendImage { get; } =
-            NativeMethods.GetCanSendImage(AuthCode);
+        public static bool CanSendImage =>
+            _canSendImage ??= NativeMethods.GetCanSendImage(AuthCode);
 
         /// <summary>
         /// 获取一个值，指示是否可以发送录音。
         /// </summary>
-        public static bool CanSendRecord { get; } =
-            NativeMethods.GetCanSendRecord(AuthCode);
+        public static bool CanSendRecord =>
+            _canSendRecord ??= NativeMethods.GetCanSendRecord(AuthCode);
 
         /// <summary>
         /// 获取当前用户。
         /// </summary>
-        public static CurrentUser CurrentUser { get; } = new CurrentUser();
+        public static CurrentUser CurrentUser =>
+            _currentUser ??= new CurrentUser();
 
         internal static object App { get; private set; }
 
@@ -72,8 +78,8 @@ namespace HuajiTech.CoolQ
         /// <summary>
         /// 获取应用的数据目录。
         /// </summary>
-        public static DirectoryInfo DataDirectory { get; } =
-            new DirectoryInfo(NativeMethods.GetDataDirectory(AuthCode));
+        public static DirectoryInfo DataDirectory =>
+            _dataDirectory ??= new DirectoryInfo(NativeMethods.GetDataDirectory(AuthCode));
 
         /// <summary>
         /// 请求图片。
