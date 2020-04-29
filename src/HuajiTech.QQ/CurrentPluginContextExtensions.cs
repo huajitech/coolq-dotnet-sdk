f786bad050ccbec8687345d287268a3e7ae206d1
SYC <yc.s@outlook.com>
2020-04-29 15:03:49 +0800

    添加扩展方法。

--- src/HuajiTech.QQ/CurrentPluginContextExtensions.cs
+++ src/HuajiTech.QQ/CurrentPluginContextExtensions.cs
@@ -1,23 +1,20 @@
-﻿namespace HuajiTech.QQ
+﻿using System;
+
+namespace HuajiTech.QQ
 {
     /// <summary>
     /// 定义用于操作 <see cref="PluginContext.Current"/> 的扩展方法。
     /// </summary>
     public static class CurrentPluginContextExtensions
     {
-        public static User AsUser(this User user)
-        {
-            return PluginContext.Current.GetUser(user);
-        }
+        public static User AsUser(this User user) => PluginContext.Current.GetUser(user);
 
-        public static Member AsMemberOf(this User user, Group group)
-        {
-            return PluginContext.Current.GetMember(user, group);
-        }
+        public static Member AsMemberOf(this User user, Group group) => PluginContext.Current.GetMember(user, group);
 
-        public static Member AsMemberOf(this User user, long groupNumber)
-        {
-            return PluginContext.Current.GetMember(user, groupNumber);
-        }
+        public static Member AsMemberOf(this User user, long groupNumber) => PluginContext.Current.GetMember(user, groupNumber);
+
+        public static void LogAsWarning(this Exception ex) => PluginContext.Current.Bot.Logger.LogWarning(ex);
+
+        public static void LogAsError(this Exception ex) => PluginContext.Current.Bot.Logger.LogError(ex);
     }
 }
