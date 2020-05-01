2f2f98bc2182c1178fb629c9023ee53c338a7f1c
SYC <yc.s@outlook.com>
2020-05-01 16:26:23 +0800

    更新文档。

--- src/HuajiTech.QQ/IChattable.cs
+++ src/HuajiTech.QQ/IChattable.cs
@@ -2,10 +2,19 @@
 
 namespace HuajiTech.QQ
 {
+    /// <summary>
+    /// 定义可聊天的对象。
+    /// </summary>
     public interface IChattable : ISendable, IEquatable<IChattable>
     {
+        /// <summary>
+        /// 获取当前 <see cref="IChattable"/> 对象的显示名称。
+        /// </summary>
         string DisplayName { get; }
 
+        /// <summary>
+        /// 获取当前 <see cref="IChattable"/> 对象的号码。
+        /// </summary>
         long Number { get; }
     }
 }
--- src/HuajiTech.QQ/IMember.cs
+++ src/HuajiTech.QQ/IMember.cs
@@ -1,7 +1,13 @@
 ﻿namespace HuajiTech.QQ
 {
+    /// <summary>
+    /// 定义成员。
+    /// </summary>
     public interface IMember
     {
+        /// <summary>
+        /// 获取当前 <see cref="IMember"/> 对象的所在群。
+        /// </summary>
         IChattable Group { get; }
     }
 }
